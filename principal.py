576d36f48076a6011c50dfaa3498b857ab6497a7
pedroDeseadoNovafinkas <pedro.deseado@novafinkas.es>
2024-12-15 19:30:49 +0100

    Versión 0.4 Tabla TrieView

--- principal.py
+++ principal.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+
 import customtkinter as ctk
 import xlrd
 import glob
 import pandas as pd
+from pandastable import Table, TableModel, config
 from comunidades import *
 
 def centrarPantalla(windows, factor):
@@ -22,10 +25,39 @@ def configurar_regilla(windows):
     app.grid_columnconfigure(0, weight=1)  # Primera columna (20% del ancho)
     app.grid_columnconfigure(1, weight=9)  # Segunda columna (80% del ancho)
 
+def limpiar_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+    return frame
 
-def radioButton_event(comunidad_seleccionada, ruta_seleccionada):
+def aumentar_tabla(tabla, veces):
+    for i in range(1, veces):
+        tabla.zoomIn()
+    return tabla
+
+def radioButton_event(comunidad_seleccionada, ruta_seleccionada, framePrincipal):
     print(f"Seleccionada {comunidad_seleccionada} hallada en fichero {ruta_seleccionada}")
+    framePrincipal= limpiar_frame(framePrincipal)
 
+    df = pd.read_excel(ruta_seleccionada, skiprows= 8, usecols=["F. Operativa","Concepto","Importe","Saldo"], engine='xlrd')
+
+    style = ttk.Style()
+    style.configure("Treeview", rowheight=40, font=("Arial", 18))  # Fuente para las celdas
+    style.configure("Treeview.Heading", font=("Arial", 24, "bold"))  # Fuente para los encabezados
+
+    tabla = ttk.Treeview(framePrincipal, columns=list(df.columns), show="headings", height=600)
+
+    tabla.heading('F. Operativa', text="Fecha")
+    tabla.column('F. Operativa', width = 75, anchor = 'c')
+    tabla.heading('Concepto', text="Concepto")
+    tabla.column('Concepto',  width = 575, anchor = 'w')
+    tabla.heading('Importe', text="Importe")
+    tabla.column('Importe',  width = 50, anchor = 'center')
+    tabla.heading('Saldo', text="Saldo")
+    tabla.column('Saldo',  width = 50, anchor = 'center')
+    for _, row in df.iterrows():
+        tabla.insert("", "end", values=list(row))
+    tabla.pack(fill='both', expand=True)
 
 def cargar_dataframe_comunidades(ruta_carpeta):
     df_comunidades = pd.DataFrame(columns=["NombreCorto", "Ruta"])
@@ -40,24 +72,26 @@ def cargar_dataframe_comunidades(ruta_carpeta):
     return df_comunidades
 
 
-def generar_botones_radio(df_comunidades):
+def generar_botones_radio(df_comunidades, frame):
     radio_var = tk.IntVar(value=0)
     contador = 0
     for comunidad in df_comunidades["NombreCorto"].unique():
         ctk.CTkRadioButton(frameLateral, text=comunidad,
-                           command=lambda com_selec=comunidad, ruta_selec=
-                           df_comunidades.loc[df_comunidades["NombreCorto"] == comunidad].values[0][1]:
-                           radioButton_event(com_selec, ruta_selec),
+                           command=lambda com_selec=comunidad,
+                           ruta_selec = df_comunidades.loc[df_comunidades["NombreCorto"] == comunidad].values[0][1],
+                           framePrincipal = frame:
+                           radioButton_event(com_selec, ruta_selec, framePrincipal),
                            variable=radio_var, value=contador).pack(padx=10, pady=5, anchor="w")
         contador += 1
 
 
-def cargar_comunidades():
+def cargar_comunidades(boton_carga, frame):
     initial_dir = r'C:\Users\Pedro Novafinkas\OneDrive\PEDRO'
     ruta_carpeta = filedialog.askdirectory(initialdir=initial_dir)
     if ruta_carpeta:
         df_comunidades = cargar_dataframe_comunidades(ruta_carpeta)
-        generar_botones_radio(df_comunidades)
+        generar_botones_radio(df_comunidades, frame)
+        boton_carga.configure(state="disabled")
 
 
 def generar_frame_lateral(ventana_padre):
@@ -70,33 +104,42 @@ def generar_frame_lateral(ventana_padre):
 
 def generar_frame_principal(ventana_padre):
     framePrincipal = ctk.CTkScrollableFrame(ventana_padre, corner_radius=0, fg_color="lightblue")
+    #framePrincipal = tk.Frame(ventana_padre)
     framePrincipal.grid(row=0, column=1, sticky="nsew")
-    label2 = ctk.CTkLabel(framePrincipal, text="Área Superior", font=("Arial", 14))
-    label2.pack(pady=20, padx=20)
+    return framePrincipal
 
 
-def generar_frame_botones(ventana_padre):
+
+def generar_frame_botones(ventana_padre, frame):
     frameBotones = ctk.CTkFrame(ventana_padre, corner_radius=0)
     frameBotones.grid(row=1, column=1, sticky="nsew")
     label3 = ctk.CTkLabel(frameBotones, text="Área Inferior", font=("Arial", 14))
     label3.pack(pady=10, padx=10)
+    cargar_botones(ventana_padre, frame)
 
 
-def cargar_botones(frameBotones):
-    ctk.CTkButton(frameBotones, text="Seleccionar carpeta de trabajo",
-                                 command=cargar_comunidades).grid(row=1, column=1,pady=5, padx=10, sticky="w")
+def cargar_botones(frameBotones, frame):
+    boton_carga = ctk.CTkButton(frameBotones, text="Seleccionar carpeta de trabajo",
+                                 command=lambda: cargar_comunidades(boton_carga, frame))
+    boton_carga.grid(row=1, column=1,pady=5, padx=10, sticky="w")
+
 
 def principal(ventana_padre):
     ventana_padre.title("Balance Novafinkas")
-    centrarPantalla(ventana_padre, 0.60)
+    centrarPantalla(ventana_padre, 0.85)
     configurar_regilla(ventana_padre)
     frameLateral = generar_frame_lateral(ventana_padre)
     framePrincipal = generar_frame_principal(ventana_padre)
-    frameBotones = generar_frame_botones(ventana_padre)
-    cargar_botones(frameBotones)
+    frameBotones = generar_frame_botones(app, framePrincipal)
+
     return frameLateral, framePrincipal, frameBotones
 
+'''
+Empieza aquí el programa principal
+'''
 
 app = ctk.CTk()
 frameLateral, framePrincipal, frameBotones = principal(app)
+
+
 app.mainloop()
